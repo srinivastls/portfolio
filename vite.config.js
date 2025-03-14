771d853b0b21c969d9f9bddf8c27293701e8bfbf
srinivastls <lakshmisrinivas365@gmail.com>
2025-03-14 10:05:37 +0530

    change1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Website/",
+  base: "/portfolio/",  // Adjust base path for GitHub Pages
 })
